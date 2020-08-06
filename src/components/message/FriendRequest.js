debfdf41e8fd49777ade3005770e0f225bba040d
ChinYikMing <yikming2222@gmail.com>
2020-08-06 13:47:25 +0800

    adding reject friend feature

--- src/components/message/FriendRequest.js
+++ src/components/message/FriendRequest.js
@@ -3,9 +3,10 @@ import { auth, db } from '../firebase/config';
 import { CircularProgress } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import AddIcon from '@material-ui/icons/Add';
+import ClearIcon from '@material-ui/icons/Clear';
 
 const useStyles = makeStyles((theme) => ({
-    acceptFriendButtonStyle: {
+    buttonStyle: {
         '&:hover': {
             cursor: 'pointer'
         }
@@ -38,7 +39,7 @@ function FriendRequest() {
 
                     setfriendRequestsList(friendRequests);
 
-                    if (loading){
+                    if (loading) {
                         setLoading(false);
                         friendRequests = [];
                     }
@@ -48,6 +49,21 @@ function FriendRequest() {
             }
         })
     }, [])
+
+    const rejectFriend = (uid, username, friendUid, friendDisplayName) => {
+        //user自己的friendsRequestsList
+        const userfriendRequestsListRef = db.collection('users').doc(uid).collection('friendRequests').doc(friendUid);
+
+        //發送請求那一方的pendingFriendsRequests
+        const reqPendingFriendRequestsListRef = db.collection('users').doc(friendUid).collection('pendingFriendRequests').doc(uid);
+
+        userfriendRequestsListRef.delete()
+            .then(() => {
+                reqPendingFriendRequestsListRef.delete()
+            }).then(() => {
+                console.log(`You have rejected ${friendDisplayName}'s friend request`);
+            })
+    }
 
     const acceptFriend = (uid, username, friendUid, friendDisplayName) => {
         //user自己的friendList
@@ -99,7 +115,8 @@ function FriendRequest() {
                     {friendRequestsList.map(fr =>
                         <div className="user">
                             {fr.displayName}
-                            <AddIcon onClick={() => acceptFriend(uid, username, fr.uid, fr.displayName)} className={classes.acceptFriendButtonStyle} />
+                            <AddIcon onClick={() => acceptFriend(uid, username, fr.uid, fr.displayName)} className={classes.buttonStyle} />
+                            <ClearIcon onClick={() => rejectFriend(uid, username, fr.uid, fr.displayName)} className={classes.buttonStyle} />
                         </div>
                     )}
                 </div>
